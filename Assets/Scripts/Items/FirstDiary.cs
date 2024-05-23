a229e3aa14ae92489c9547c5e9352d8717cbeb48
최경선 <cgs@op.gg>
2024-05-23 19:26:02 +0900

    feat: add resources

---
+++ Assets/Scripts/Items/FirstDiary.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstDiary : MonoBehaviour
+{
+    private bool triggerOn = false;
+    private PlayerMove thePlayer;
+    public GameObject Canvas;
+    public GameObject Inventory;
+    public GameObject Text;
+    private bool isReadingDiary = false;
+
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerMove>();
+    }
+
+    void Update()
+    {
+        // 플레이어가 아이템과 충돌하고 특정 키를 눌렀을 때 아이템 장착
+        if (triggerOn && thePlayer.IsKeydown)
+        {
+            Debug.Log(this.name);
+            if (this.name == "first_diary")
+            {
+                Debug.Log("get a firstDiary");
+                Inventory.SetActive(false);
+                Canvas.SetActive(true);
+                Text.SetActive(true);
+                isReadingDiary = true;
+            }
+        }
+        // 플레이어가 연구 일지를 읽고 있을 때, 다른 키를 눌러 상태를 벗어나기
+        if (isReadingDiary && Input.anyKeyDown)
+        {
+            isReadingDiary = false;
+            Text.SetActive(false);
+            Canvas.SetActive(false);
+            Inventory.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player") // Equals로 변경
+        {
+            Debug.Log("triggerOn is false");
+            triggerOn = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Player") // Equals로 변경
+        {
+            Debug.Log("triggerOn is true");
+            triggerOn = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Items/SecondDoor.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondDoor : MonoBehaviour
+{
+    private bool isNearDoor = false;
+    private bool isLock = true;
+    private PlayerMove thePlayer;
+    public GameObject Wall;
+
+    private void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerMove>();
+    }
+
+    private void Update()
+    {
+        if (isNearDoor && Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log(isNearDoor);
+            if (isLock)
+            {
+                // 문을 여는 로직 (예: 문을 비활성화하거나 다른 씬으로 이동)
+                Debug.Log("The door is opened with the silver key!");
+                Wall.SetActive(false);
+                isLock = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("!!!!!!!!!!!!!!!!!");
+        if (other.CompareTag("Player"))
+        {
+            isNearDoor = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isNearDoor = false;
+        }
+    }
+}
